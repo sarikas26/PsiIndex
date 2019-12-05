b72e2d139580e3cb6af11c07d33bdaad253346af
Sarika Singh <sarika.singh@capgemini.com>
2019-12-05 23:37:37 +0530

    added dependency for google maps and loading map fragment in activity

--- app/src/main/java/com/capgemini/sarikin/psiindex/MainActivity.kt
+++ app/src/main/java/com/capgemini/sarikin/psiindex/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.capgemini.sarikin.psiindex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.SupportMapFragment
 
+/**
+ * Created by Sarika Singh
+ */
 class MainActivity : AppCompatActivity() {
+
+    private var mapFragment: SupportMapFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = getString(R.string.app_title)
+
+        mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
     }
 }
